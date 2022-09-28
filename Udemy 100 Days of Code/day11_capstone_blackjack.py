7ef226f89f74e665858c3819a7c11b29212f4071
Johan <jg2johan@gmail.com>
2022-09-28 12:11:43 +0100

    Added day 11 project
    Blackjack capstone project

---
+++ Udemy 100 Days of Code/day11_capstone_blackjack.py
@@ -0,0 +1,42 @@
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+hand = []
+score = 0
+end = False
+
+# Drawing card and changing Ace to 1 if score exceeds 21
+def draw(score):
+    draw = random.choice(cards)
+    if score + draw > 21 and draw == 11:
+        draw = 1
+    return draw
+
+def hand_score():
+    score = sum(hand)
+    return score
+
+def blackjack():
+    while not end:
+        card = draw(score)
+        hand.append(card)
+        new_score = hand_score()
