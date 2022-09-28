7e18dcaa6b09f7d21f3fa8cb45d3487c59d6baa3
Johan <jg2johan@gmail.com>
2022-09-28 23:01:38 +0100

    Completed day 11 capstone project

--- Udemy 100 Days of Code/day11_capstone_blackjack.py
+++ Udemy 100 Days of Code/day11_capstone_blackjack.py
@@ -16,13 +16,35 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
+# Player draws first
 
 import random
+import os
+import day11_capstone_blackjackart as art
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-hand = []
-score = 0
 end = False
+game_round = 0
+
+
+
+
+def clear_screen():
+    os.system('cls||clear')
+
+def start():
+    dealer_first_card = 0
+    dealer_score = 0
+    player_score = 0
+    for i in range(4):
+        draw = random.choice(cards)
+        if i == 1:
+            dealer_first_card = draw
+        if (i+1) % 2 == 0:
+            dealer_score += draw
+        else:
+            player_score += draw
+    return player_score, dealer_score, dealer_first_card
 
 # Drawing card and changing Ace to 1 if score exceeds 21
 def draw(score):
@@ -31,12 +53,79 @@ def draw(score):
         draw = 1
     return draw
 
-def hand_score():
-    score = sum(hand)
-    return score
+def draw_again():
+    while True:
+        draw = input('Do you want to "stand" or "hit"? ').lower()
+        if draw == "hit":
+            return False
+        elif draw == "stand":
+            return True
+        else:
+            print('Please type "stand" or "hit"')
+
+def dealer_draw(dealer_score):
+    while dealer_score < 17:
+        dealer_score += random.choice(cards)
+    return dealer_score
+    
+def win_check(player_score, dealer_score):
+    if player_score > 21:
+        return False
+    elif player_score > dealer_score:
+        return True
+    elif dealer_score > 21:
+        return True
+    else:
+        return False
+
+def check_natural(player_score, dealer_score):
+    if player_score == 21:
+        return 1
+    elif player_score == 21 and dealer_score == 21:
+        return 2
+    elif dealer_score == 21:
+        return 3
+
+def play_again():
+    while True:
+        again = input("Would you like to play again? Y/N\n").lower()
+        if again == "y":
+            clear_screen()
+            blackjack()
+        elif again == "n":
+            print("Thanks for playing! Goodbye!")
+            return
+        else:
+            print('Please type "Y" or "N"')
 
 def blackjack():
-    while not end:
-        card = draw(score)
-        hand.append(card)
-        new_score = hand_score()
+    print(art.logo)
+    player_score, dealer_score, dealer_first_card = start()
+    natural = check_natural(player_score, dealer_score)
+    if natural == 1:
+        print("Blackjack! You win!")
+    elif natural == 2:
+        print("You and the dealer both got a blackjack! Tie!")
+    elif natural == 3:
+        print("The dealer got a blackjack! You lose.")
+    else:
+        print(f"Your starting score is {player_score} and the dealer's first card is {dealer_first_card}")
+        end = draw_again()
+        while not end:
+            card = draw(player_score)
+            player_score += card
+            if player_score >= 21:
+                end = True
+            print(f"Your score is {player_score}")
+            if player_score < 21:
+                end = draw_again()
+        dealer_score = dealer_draw(dealer_score)
+        print(f"The dealer's final score is {dealer_score}")
+        win = win_check(player_score, dealer_score)
+        if win == True:
+            print("You win!")
+        else:
+            print("You lose")
+        play_again()
+
+blackjack()
