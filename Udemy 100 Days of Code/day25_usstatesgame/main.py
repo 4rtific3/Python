fb17274d039cf7b8dd430d1b64d71e7b6dba48fe
Johan <jg2johan@gmail.com>
2022-10-11 15:46:44 +0100

    Updated day 25 project
    Applied list comprehension

--- Udemy 100 Days of Code/day25_usstatesgame/main.py
+++ Udemy 100 Days of Code/day25_usstatesgame/main.py
@@ -6,7 +6,6 @@ FONT = ("Arial", 9, "normal")
 data = pd.read_csv("50_states.csv")
 states_list = data.state.to_list()
 guessed_states = []
-missed_states = []
 
 score = 0
 
@@ -23,7 +22,15 @@ t.shape(image)
 while score < 50:
     answer_state = screen.textinput(title=f"{score}/50 Guess the State", prompt="What's another state name?").title()
     if answer_state == "Exit":
+        # for state in states_list:
+        #     if state not in guessed_states:
+        #         missed_states.append(state)
+
+        missed_states = [state for state in states_list if state not in guessed_states]
+        
+        pd.DataFrame(missed_states).to_csv("states_to_learn.csv")
         break
+    
     if answer_state in states_list and answer_state not in guessed_states:
         guessed_states.append(answer_state)
         score += 1
@@ -31,10 +38,6 @@ while score < 50:
         state_name.goto(int(state_row.x), int(state_row.y))
         state_name.write(answer_state, align="center", font=FONT)
 
-for state in states_list:
-    if state not in guessed_states:
-        missed_states.append(state)
 
-pd.DataFrame(missed_states).to_csv("states_to_learn.csv")
 
 screen.exitonclick()
