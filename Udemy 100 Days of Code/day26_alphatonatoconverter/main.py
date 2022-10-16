7fb41f18513db11c2a8f8f7e5f64c2543bcd00f1
Johan <jg2johan@gmail.com>
2022-10-16 14:36:45 +0100

    Updated day 26 project
    Added error handling with try, except, else

--- Udemy 100 Days of Code/day26_alphatonatoconverter/main.py
+++ Udemy 100 Days of Code/day26_alphatonatoconverter/main.py
@@ -4,7 +4,13 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-convert = input("Enter a word: ").upper()
-result = [nato_dict[i] for i in convert]
-
-print(result)
+while True:
+    convert = input("Enter a word: ").upper()
+    
+    try:
+        result = [nato_dict[i] for i in convert]
+    except KeyError:
+        print("Only letters in the alphabet please")
+    else:
+        print(result)
+        break
